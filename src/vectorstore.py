d6015f636c51a56afdafd0b82383c08e36e27d50
Puneeth0106 <puneethkumaramudala7@gmail.com>
2026-05-11 08:48:32 -0400

    Added astradb

---
+++ src/vectorstore.py
@@ -0,0 +1,14 @@
+import os
+from langchain_astradb import AstraDBVectorStore
+from src.config import COLLECTION_NAME
+
+
+def get_vector_store(embedding_model, final_chunks):
+    vector_store = AstraDBVectorStore(
+        collection_name=COLLECTION_NAME,
+        embedding=embedding_model,
+        api_endpoint=os.getenv('ASTRA_DB_API_ENDPOINT'),
+        token=os.getenv("ASTRA_DB_APPLICATION_TOKEN"),
+    )
+    vector_store.add_documents(documents=final_chunks)
+    return vector_store
