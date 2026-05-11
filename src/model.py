6769be839b59c3017a80d89fd21bb3e7b5ba5bf1
Puneeth0106 <puneethkumaramudala7@gmail.com>
2026-05-11 10:09:36 -0400

    Modified so that load_dotenv directly colled

--- src/model.py
+++ src/model.py
@@ -3,9 +3,8 @@ from langchain.chat_models import init_chat_model
 from langchain_openai.embeddings import OpenAIEmbeddings
 from src.config import CHAT_MODEL_NAME, TEMPERATURE, TIMEOUT,MAX_RETRIES,MAX_TOKENS,EMBEDDING_MODEL
 
-load_dotenv()
-
 def chat_model():
+    load_dotenv()
     model = init_chat_model(
         CHAT_MODEL_NAME,
         temperature=TEMPERATURE,
@@ -16,5 +15,6 @@ def chat_model():
     return model
 
 def embedding_model():
+    load_dotenv()
     embedding_model= OpenAIEmbeddings(model=EMBEDDING_MODEL)
     return embedding_model
--- src/vectorstore.py
+++ src/vectorstore.py
@@ -3,12 +3,11 @@ from langchain_astradb import AstraDBVectorStore
 from src.config import COLLECTION_NAME
 
 
-def get_vector_store(embedding_model, final_chunks):
+def get_vector_store(embedding_model):
     vector_store = AstraDBVectorStore(
         collection_name=COLLECTION_NAME,
         embedding=embedding_model,
         api_endpoint=os.getenv('ASTRA_DB_API_ENDPOINT'),
         token=os.getenv("ASTRA_DB_APPLICATION_TOKEN"),
     )
-    vector_store.add_documents(documents=final_chunks)
     return vector_store
