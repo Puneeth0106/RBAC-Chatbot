14518d2e6f4cd00bbe31e9347a6301489a4520f1
Puneeth0106 <puneethkumaramudala7@gmail.com>
2026-05-12 08:29:23 -0400

    created new pipeline include metadata user role

--- src/indexing.py
+++ src/indexing.py
@@ -1,6 +1,10 @@
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter, MarkdownHeaderTextSplitter
 from src.config import DATA_PATH,GLOB_PATTERN, CHUNK_OVERLAP,CHUNK_SIZE
+from langchain_astradb import AstraDBVectorStore
+from src.config import COLLECTION_NAME
+from src.model import embedding_model
+import os
 
 def loading_docs(path):
     loader= DirectoryLoader(
@@ -12,16 +16,20 @@ def loading_docs(path):
     return docs
 
 
+#Splitting
+headers_to_split_on = [
+    ("#", "Header 1"),
+    ("##", "Header 2"),
+    ("###", "Header 3"),
+]
+
+md_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
+char_splitter= RecursiveCharacterTextSplitter(chunk_size= CHUNK_SIZE,chunk_overlap=CHUNK_OVERLAP)
+
+
+
 def splitting_docs(docs):
-    #Splitting
-    headers_to_split_on = [
-        ("#", "Header 1"),
-        ("##", "Header 2"),
-        ("###", "Header 3"),
-    ]
     md_chunks=[]
-    md_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
-    char_splitter= RecursiveCharacterTextSplitter(chunk_size= CHUNK_SIZE,chunk_overlap=CHUNK_OVERLAP)
     for doc in docs:
         md_chunks.extend(md_splitter.split_text(doc.page_content))
     final_chunks = char_splitter.split_documents(md_chunks)
@@ -29,5 +37,22 @@ def splitting_docs(docs):
 
 
 
+if __name__== "__main__":
 
+    vector_store = AstraDBVectorStore(
+    collection_name=COLLECTION_NAME,
+    embedding=embedding_model(),
+    api_endpoint=os.getenv('ASTRA_DB_API_ENDPOINT'),
+    token=os.getenv("ASTRA_DB_APPLICATION_TOKEN"))
+
+    roles= ["engineering", "finance", "marketing","hr", "general"]
+
+    for role in roles:
+        path= f"{DATA_PATH}/{role}"
+        docs= loading_docs(path)
+        chunks= splitting_docs(docs)
+        for chunk in chunks:
+            chunk.metadata["role"] = role
+
+        vector_store.add_documents(chunks)
 
