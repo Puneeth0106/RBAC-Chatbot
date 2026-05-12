3108731f396993f33e4ecefd36041a6477c27569
Puneeth0106 <puneethkumaramudala7@gmail.com>
2026-05-12 08:48:03 -0400

    Bill chain accepts the role and it is passed to the retriever, where it is used as a filter.

--- src/chain.py
+++ src/chain.py
@@ -7,9 +7,9 @@ from langchain_core.runnables import RunnablePassthrough, RunnableParallel, Runn
 from langchain_core.output_parsers import StrOutputParser
 
 # Retriver
-def build_retriever(embedding_model):
+def build_retriever(embedding_model, user_role):
     vector_store= get_vector_store(embedding_model)
-    retriever = vector_store.as_retriever(search_kwargs={"k":RETRIEVER_K })
+    retriever = vector_store.as_retriever(search_kwargs={"k":RETRIEVER_K , 'filter': {'role':{'$in': [user_role, "general"]}}})
     return retriever
 
 def build_prompt():
@@ -22,13 +22,15 @@ def build_prompt():
     return prompt
 
 def format_output(docs):
+    # for doc in docs:
+    #     print(doc.metadata)
     return  "\n\n".join(doc.page_content for doc in docs)
 
-def build_chain():
+def build_chain(user_role):
     parallel_chain= RunnableParallel(
         {
             'question': RunnablePassthrough(),
-            'context': build_retriever(embedding_model()) | RunnableLambda(format_output)
+            'context': build_retriever(embedding_model(),user_role) | RunnableLambda(format_output)
         }
     )
     final_chain= parallel_chain | build_prompt() | chat_model() | StrOutputParser()
@@ -36,4 +38,4 @@ def build_chain():
 
 
 if __name__== "__main__":
-    print(build_chain().invoke("How many leaves can an employee get"))
+    print(build_chain('engineering').invoke("What CI/CD pipelines does FinSolve use?"))
