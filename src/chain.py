e39b8a4ef3f93004560873279dc5caa37fb84a42
Puneeth0106 <puneethkumaramudala7@gmail.com>
2026-05-12 08:54:55 -0400

    Optimized chain.py to include chat and embedding  models as modular  level components

--- src/chain.py
+++ src/chain.py
@@ -6,9 +6,13 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough, RunnableParallel, RunnableLambda
 from langchain_core.output_parsers import StrOutputParser
 
+CHAT= chat_model()
+EMBEDDING= embedding_model()
+
+
 # Retriver
-def build_retriever(embedding_model, user_role):
-    vector_store= get_vector_store(embedding_model)
+def build_retriever(embedding, user_role):
+    vector_store= get_vector_store(embedding)
     retriever = vector_store.as_retriever(search_kwargs={"k":RETRIEVER_K , 'filter': {'role':{'$in': [user_role, "general"]}}})
     return retriever
 
@@ -22,18 +26,16 @@ def build_prompt():
     return prompt
 
 def format_output(docs):
-    # for doc in docs:
-    #     print(doc.metadata)
     return  "\n\n".join(doc.page_content for doc in docs)
 
 def build_chain(user_role):
     parallel_chain= RunnableParallel(
         {
             'question': RunnablePassthrough(),
-            'context': build_retriever(embedding_model(),user_role) | RunnableLambda(format_output)
+            'context': build_retriever(EMBEDDING,user_role) | RunnableLambda(format_output)
         }
     )
-    final_chain= parallel_chain | build_prompt() | chat_model() | StrOutputParser()
+    final_chain= parallel_chain | build_prompt() | CHAT | StrOutputParser()
     return final_chain
 
 
