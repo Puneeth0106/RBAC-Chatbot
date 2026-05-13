e227baee5c06bd48ecbb3f4b7309c8c411e14df2
Puneeth0106 <puneethkumaramudala7@gmail.com>
2026-05-13 08:04:31 -0400

    Build a chat response model for a FastAPI.

--- app/schemas/chat.py
+++ app/schemas/chat.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel, Field
-from typing import Annotated
 
 class ChatRequest(BaseModel):
     message : str = Field(description="Message sent by the user")
 
 
+class ChatResponse(BaseModel):
+    answer : str = Field(description="Answer responsed by the AI model")
