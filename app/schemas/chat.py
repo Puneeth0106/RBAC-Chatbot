0d7d1513e836cd8c51eefcae071b2bd011c377c3
Puneeth0106 <puneethkumaramudala7@gmail.com>
2026-05-14 10:16:48 -0400

    Fixed the typo error.

--- app/schemas/chat.py
+++ app/schemas/chat.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field
 
 class ChatRequest(BaseModel):
     message : str = Field(description="Message sent by the user")
-    sessionid : str = Field(description="Session Id for each individual chat and holds all the message for that converstaion")
+    session_id : str = Field(description="Session Id for each individual chat and holds all the message for that converstaion")
 
 
 class ChatResponse(BaseModel):
