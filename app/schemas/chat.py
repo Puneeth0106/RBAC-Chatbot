2fc3bcfb18be43275ebd5a387a7d9b92f2cbe2fe
Puneeth0106 <puneethkumaramudala7@gmail.com>
2026-06-09 06:23:30 -0400

    Guardrail1- input validation

--- app/schemas/chat.py
+++ app/schemas/chat.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field , StringConstraints
+from typing import Annotated
 
 class ChatRequest(BaseModel):
-    message : str = Field(description="Message sent by the user")
-    session_id : str = Field(description="Session Id for each individual chat and holds all the message for that converstaion")
-
+    message : Annotated[str,Field(description="Message sent by the user") , StringConstraints(strip_whitespace= True, min_length=1, max_length=2000)]
+    session_id :Annotated[str,Field(description="Session Id for each individual chat and holds all the message for that converstaion", min_length=1, max_length=100)]
 
