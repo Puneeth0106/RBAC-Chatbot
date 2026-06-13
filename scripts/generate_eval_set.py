3f166f03d6b7e456a027a1c67d83ec5798a93695
Puneeth0106 <puneethkumaramudala7@gmail.com>
2026-06-13 18:55:47 -0400

    created ragas script for generating test cases

---
+++ scripts/generate_eval_set.py
@@ -0,0 +1,21 @@
+from app.services.config import DATA_PATH
+from app.services.indexing import loading_docs
+from app.services.model import chat_model, embedding_model
+from ragas.testset import TestsetGenerator
+import pandas as pd
+
+generator= TestsetGenerator.from_langchain(chat_model(),embedding_model())
+
+
+frames= []
+
+for role in ["engineering", "finance", "marketing","hr", "general"]:
+    path= f'{DATA_PATH}/{role}'
+    docs=loading_docs(path)
+    testset= generator.generate_with_langchain_docs(docs,testset_size=10)
+    df = testset.to_pandas()
+    df['role']= role
+    frames.append(df)
+
+pd.concat(frames).to_csv(f"resources/eval/golden_raw.csv", index= False)
+
